1ddb1da5a3bdd944bb4da59484eee7806ad013fb
raoqiyu <raoqiyu@yeah.net>
2015-10-16 22:35:03 +0800

    340 ms,  beats 7.81% of python submissions

---
+++ 009-Palindrome-Number/Solution 1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        for i in range(len(str(x))//2):
+            if str(x)[i] != str(x)[len(str(x))-i-1]:
+                return False
+        return True
