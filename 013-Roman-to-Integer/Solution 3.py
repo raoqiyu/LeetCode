68b2da762c7cf01d35ab5e3268dc72f28a892296
raoqiyu <raoqiyu@yeah.net>
2015-10-19 09:14:15 +0800

    152 ms, beats 96.59% of python submissions

---
+++ 013-Roman-to-Integer/Solution 3.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mappedDict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' :1000}
+        mappedS = [mappedDict[x] for x in s]
+        ret = sum(mappedS)
+        for index in range(len(mappedS) - 1):
+            if mappedS[index] < mappedS[index+1]:
+                ret -= 2 * mappedS[index]
+        return ret
