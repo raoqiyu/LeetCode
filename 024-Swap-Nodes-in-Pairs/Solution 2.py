5498d3a4286da97a6c12313701e210deaa484264
raoqiyu <raoqiyu@yeah.net>
2015-11-02 14:22:10 +0800

    40 ms, beats 69.25% of python submissions

---
+++ 024-Swap-Nodes-in-Pairs/Solution 2.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Scan the linked list, each time we meet a node , 
+        #   swap the value of this node and the node after this 
+        ptr = head
+        while ptr and ptr.next:
+            ptr.val, ptr.next.val = ptr.next.val, ptr.val
+            ptr = ptr.next.next
+        return head
+        
