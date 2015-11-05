4d9a6a54a93dc8599776463c9ae90d4763b6139e
raoqiyu <raoqiyu@yeah.net>
2015-11-05 10:09:29 +0800

    4 ms, beats 6.59% of cpp submissions

---
+++ 027-Remove-Element/Solution 2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // Scan the array with two pointers
+        // ptr1 : the current position which's value is val
+        // ptr2 : the first position after ptr1 which's value is not val 
+        int ptr1 = 0, ptr2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if( nums[i] ==  val)
+                ptr2++;
+            else{
+                nums[ptr1++] = nums[ptr2++];
+            }
+        }
+        return ptr1;
+    }
+};
