94bfc5f54169956d2f531148ca749bb956f9d904
raoqiyu <raoqiyu@yeah.net>
2016-04-17 22:14:54 +0800

    inorder

---
+++ Solutions/098-Validate-Binary-Search-Tree/Solution1.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        // Traverse the tree in inorder
+        if(root != NULL){
+            if(!isValidBST(root->left)) return false;
+            
+            if(prev != NULL && root->val <= prev->val) return false;
+            prev = root;
+            
+            return isValidBST(root->right); 
+            
+        }
+        return true;
+    }
+private:
+    TreeNode *prev=NULL;
+};
