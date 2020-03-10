47db1ed84dc688a30a47ed12d760fbe1d3b2382b
raoqiyu <raoqiyu@yeah.net>
2020-03-10 10:39:10 +0800

    two pointers

---
+++ Solutions/328. Odd Even Linked List/Solution1.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return
+        odd, even, even_head = head, head.next, head.next
+        
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
